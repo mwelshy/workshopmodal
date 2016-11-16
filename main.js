08aab5bb2ad58a68fc77ede964d470fe4400c04e
Michael Welsh <brohemian1989@gmail.com>
2016-11-16 15:59:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById("myModal");
+document.getElementById("myBtn").addEventListener("onclick", displayit);
+
+function displayit(){
+
+	var modal = document.getElementById("myModal");
+	modal.style.display = "block";
+}
