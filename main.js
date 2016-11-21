054699ad1fe93dab41a9d2d5fd3d5ae05e6488aa
Michael Welsh <brohemian1989@gmail.com>
2016-11-20 20:06:25 -0500

    cleaned up code

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+//  selects modal
+
 
 var modal = document.getElementById("myModal");
+
+//  adds functionality to X 
 var span = document.getElementsByTagName("span")[1].addEventListener("click", closeit);
 
+
+//  shows the modal in display: block
 function displayit(){
 	modal.style.display = "block";
 
 }
 
+// allows X to display: none
 function closeit() {
 	modal.style.display = "none";
 }
 
 
+//  gives a 3 second delay on the page before the modal opens. 
 window.setTimeout(displayit, 3000);
+
